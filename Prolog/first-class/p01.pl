1786e53d182501fd7b70ff97b7d74d90e10071c7
sirkotal <lordkotal2002@gmail.com>
2023-09-19 15:53:29 +0100

    Added the parent relations

--- Prolog/first-class/p01.pl
+++ Prolog/first-class/p01.pl
@@ -26,3 +26,34 @@ male(luke).
 male(rexford).
 male(calhoun).
 male(george).
+
+parent(grace, phil).
+parent(frank, phil).
+parent(dede, claire).
+parent(dede, mitchell).
+parent(jay, claire).
+parent(jay, mitchell).
+parent(jay, joe).
+parent(gloria, joe).
+parent(gloria, manny).
+parent(javier, manny).
+parent(barb, cameron).
+parent(barb, pameron).
+parent(merle, cameron).
+parent(merle, pameron).
+parent(phil, haley).
+parent(phil, alex).
+parent(phil, luke).
+parent(claire, haley).
+parent(claire, alex).
+parent(claire, luke).
+parent(mitchell, lily).
+parent(mitchell, rexford).
+parent(cameron, lily).
+parent(cameron, rexford).
+parent(pameron, calhoun).
+parent(bo, calhoun).
+parent(dylan, george).
+parent(dylan, poppy).
+parent(haley, george).
+parent(haley, poppy).
