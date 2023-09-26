3eefe7d19084efc8780a1f338cc2b37ab8e512c1
sirkotal <lordkotal2002@gmail.com>
2023-09-26 16:27:49 +0100

    Added the Fibonacci and Is_Prime code for exercise 4

--- Prolog/p02/ex4.pl
+++ Prolog/p02/ex4.pl
@@ -15,3 +15,29 @@ somaRec(N, Sum) :-
     N1 is N - 1,
     somaRec(N1, Sum1),
     Sum is N + Sum1.
+
+/* c) */
+
+/* e) */
+
+fibonacci(0, 1).
+fibonacci(1, 1).
+fibonacci(N, F) :-
+    N > 1,
+    N1 is N - 1,
+    N2 is N - 2,
+    fibonacci(N1, F1),
+    fibonacci(N2, F2),
+    F is F1 + F2.
+
+/* g) */
+
+divisor(X, X) :- !.
+divisor(X, Y) :-
+    X > Y,
+    Y > 1,
+    (X mod Y) =\= 0,
+    Y1 is Y + 1,
+    divisor(X, Y1).
+
+is_prime(X) :- divisor(X, 2).
