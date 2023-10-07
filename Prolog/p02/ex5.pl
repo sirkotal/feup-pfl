95d248aaa7c35009557821cc1518a29c149ff185
sirkotal <lordkotal2002@gmail.com>
2023-10-07 15:17:55 +0100

    Added the remaining code for exercise 5

--- Prolog/p02/ex5.pl
+++ Prolog/p02/ex5.pl
@@ -1,5 +1,7 @@
 /* a) */
 
+/* i) */
+
 factorial(1, F, F).
 factorial(N, Acc, F) :-
   N > 1,
@@ -7,7 +9,7 @@ factorial(N, Acc, F) :-
   Acc1 is Acc * N,
   factorial(N1, Acc1, F).
 
-/* b) */
+/* ii) */
 
 sum_rec(0, X, X).
 sum_rec(N, Acc, X) :-
@@ -18,7 +20,7 @@ sum_rec(N, Acc, X) :-
 
 sum_rec(N, X) :- sum_rec(N, 0, X).
 
-/* c) */
+/* iii) */
 
 pow_tail(_, 0, Acc, Acc).
 pow_tail(X, Y, Acc, P) :-
@@ -29,3 +31,25 @@ pow_tail(X, Y, Acc, P) :-
 
 pow_rec(X, Y, P) :-
   pow_tail(X, Y, 1, P).
+
+/* iv) */
+
+square_tail(0, Acc, Acc).
+square_tail(N, Acc, S) :-
+  N > 0,
+  Acc1 is Acc + N + N - 1,
+  N1 is N - 1,
+  square_tail(N1, Acc1, S).
+
+square_rec(N, S) :- square_tail(N, 0, S).  
+
+/* v) */
+
+fibonacci_tail(0, A, _, A).
+fibonacci_tail(N, A, B, F) :-
+  N > 0,
+  N1 is N - 1,
+  Sum is A + B,
+  fibonacci_tail(N1, B, Sum, F).
+
+fibonacci(N, F) :- fibonacci_tail(N, 0, 1, F).
