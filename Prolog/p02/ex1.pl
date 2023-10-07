47f89d9bd6ce2f98aeff6c17c1dc652fbddef72e
sirkotal <lordkotal2002@gmail.com>
2023-10-07 15:23:36 +0100

    Added the code for exercise 6

--- Prolog/p02/ex1.pl
+++ Prolog/p02/ex1.pl
@@ -1,9 +1,9 @@
 /* a)
 
 i -> X = a, Y = b, Z = c
-     X = a, Y = b, Z = d
-     X = a, Y = d, Z = e
-     X = a, Y = c, Z = c
+    X = a, Y = b, Z = d
+    X = a, Y = d, Z = e
+    X = a, Y = c, Z = c
 
 ii -> X = a, Y = c
 
--- Prolog/p02/ex5.pl
+++ Prolog/p02/ex5.pl
@@ -4,19 +4,19 @@
 
 factorial(1, F, F).
 factorial(N, Acc, F) :-
-  N > 1,
-  N1 is N - 1,
-  Acc1 is Acc * N,
-  factorial(N1, Acc1, F).
+    N > 1,
+    N1 is N - 1,
+    Acc1 is Acc * N,
+    factorial(N1, Acc1, F).
 
 /* ii) */
 
 sum_rec(0, X, X).
 sum_rec(N, Acc, X) :-
-  N > 0,
-  N1 is N - 1,
-  Acc1 is Acc + N,
-  sum_rec(N1, Acc1, X).
+    N > 0,
+    N1 is N - 1,
+    Acc1 is Acc + N,
+    sum_rec(N1, Acc1, X).
 
 sum_rec(N, X) :- sum_rec(N, 0, X).
 
@@ -24,22 +24,22 @@ sum_rec(N, X) :- sum_rec(N, 0, X).
 
 pow_tail(_, 0, Acc, Acc).
 pow_tail(X, Y, Acc, P) :-
-  Y > 0,
-  Y1 is Y - 1,
-  Acc1 is Acc * X,
-  pow_tail(X, Y1, Acc1, P).
+    Y > 0,
+    Y1 is Y - 1,
+    Acc1 is Acc * X,
+    pow_tail(X, Y1, Acc1, P).
 
 pow_rec(X, Y, P) :-
-  pow_tail(X, Y, 1, P).
+    pow_tail(X, Y, 1, P).
 
 /* iv) */
 
 square_tail(0, Acc, Acc).
 square_tail(N, Acc, S) :-
-  N > 0,
-  Acc1 is Acc + N + N - 1,
-  N1 is N - 1,
-  square_tail(N1, Acc1, S).
+    N > 0,
+    Acc1 is Acc + N + N - 1,
+    N1 is N - 1,
+    square_tail(N1, Acc1, S).
 
 square_rec(N, S) :- square_tail(N, 0, S).  
 
@@ -47,9 +47,9 @@ square_rec(N, S) :- square_tail(N, 0, S).
 
 fibonacci_tail(0, A, _, A).
 fibonacci_tail(N, A, B, F) :-
-  N > 0,
-  N1 is N - 1,
-  Sum is A + B,
-  fibonacci_tail(N1, B, Sum, F).
+    N > 0,
+    N1 is N - 1,
+    Sum is A + B,
+    fibonacci_tail(N1, B, Sum, F).
 
 fibonacci(N, F) :- fibonacci_tail(N, 0, 1, F).
