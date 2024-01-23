3cf628986ce4f092b8649026b93746086433d9b6
sirkotal <lordkotal2002@gmail.com>
2024-01-23 16:29:53 +0000

    Some changes to exercises 2 and 3

--- Haskell/p03/ex02.hs
+++ Haskell/p03/ex02.hs
@@ -1,2 +1,2 @@
 dec2int :: [Int] -> Int
-dec2int = foldl (\x b -> x * 10 + b) 0
+dec2int lst = foldl (\x d -> x * 10 + d) 0 lst
--- Haskell/p03/ex03.hs
+++ Haskell/p03/ex03.hs
@@ -1,4 +1,4 @@
-zipWithRec :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWithRec f [] x = []
-zipWithRec f x [] = []
-zipWithRec f (x : xr) (y : yr) = f x y : zipWithRec f xr yr
+myzipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myzipWith f [] _ = []
+myzipWith f _ [] = []
+myzipWith f (x:xs) (y:ys) = (f x y:myzipWith f xs ys) 
